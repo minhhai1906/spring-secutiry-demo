535593b9876a6525b93f0203583d04296261931c
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-03-10 01:28:12 +0700

    apply bootstrap

--- src/main/java/com/minhhai/springsecurity/demo/controller/LoginController.java
+++ src/main/java/com/minhhai/springsecurity/demo/controller/LoginController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
     @GetMapping("/showMyLoginPage")
-    public String showMyLoginpage(){
-        return "plain-login";
+    public String showMyLoginPage(){
+//        return "plain-login";
+        return "fancy-login";
     }
 }
